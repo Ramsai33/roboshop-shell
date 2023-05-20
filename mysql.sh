e6ae4ad88a8e84fe1c3d8a678b47721dbd262067
Ramsai35 <mudurukollaramsai335@gmail.com>
2023-05-20 13:55:43 +0530

    mysql

---
+++ mysql.sh
@@ -0,0 +1,25 @@
+source common.sh
+if [ -z "${set_root_password}" ]; then
+  echo "Please enter mysql Password"
+  exit
+fi
+
+print_head "Disable Module"
+yum module disable mysql -y &>>${LOG}
+status_check
+
+print_head "Copying Repo"
+cp ${script_location}/files/mysql.repo /etc/yum.repos.d/mysql.repo &>>${LOG}
+status_check
+
+print_head "Installing Mysql"
+yum install mysql-community-server -y &>>${LOG}
+status_check
+
+print_head
+systemctl enable mysqld &>>${LOG}
+systemctl start mysqld &>>${LOG}
+status_check
+
+mysql_secure_installation --set-root-pass ${set_root_password}
+
